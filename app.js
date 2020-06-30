85b766bc9f10605ec739e9d2366859e0b6a53f62
Ashkan Mohseni <amohseni80@yahoo.com>
2020-06-30 18:49:26 -0400

    Updates the new JavaScript

--- app.js
+++ app.js
@@ -7,37 +7,66 @@ const headline = document.querySelector('.headline');
 const tl = new TimelineMax();
 
 tl.fromTo(
-  hero,
-  1,
-  {height:"10%"},
-  {height:'80%', ease: Power2.easeInOut}
+    hero,
+    1, {
+        height: "10%"
+    }, {
+        height: '80%',
+        ease: Power2.easeInOut
+    }
 ).fromTo(
-  hero,
-  1.2,
-  {width:"100%"},
-  {width:"80%", ease:Power2.easeInOut}
+    hero,
+    1.2, {
+        width: "100%"
+    }, {
+        width: "80%",
+        position: "relative",
+        left: "10",
+        transform: "translate(10%)",
+        ease: Power2.easeInOut
+    }
 ).fromTo(
-  slider,
-  1.2,
-  {x:"-100%"},
-  {x:"0%", ease:Power2.easeInOut},
-  "-=1.2"
+    slider,
+    1.2, {
+        x: "-100%"
+    }, {
+        x: "0%",
+        ease: Power2.easeInOut
+    },
+    "-=1.2"
 ).fromTo(
-  logo,
-  0.5,
-  {opacity:0, x:30},
-  {opacity:1, x:0},
-  "-=0.5"
+    logo,
+    0.5, {
+        opacity: 0,
+        x: 30
+    }, {
+        opacity: 1,
+        x: 0
+    },
+    "-=0.5"
 ).fromTo(
-  menu,
-  0.5,
-  {opacity:0, x:30},
-  {opacity:1, x:0},
-  "-=0.5"
+    menu,
+    0.5, {
+        opacity: 0,
+        x: 30
+    }, {
+        opacity: 1,
+        x: 0
+    },
+    "-=0.5"
 ).fromTo(
-  headline,
-  0.5,
-  {opacity:0, x:30},
-  {opacity:1, x:0},
-  "-=0.5"
+    headline,
+    0.5, {
+        opacity: 0,
+        x: 30
+    }, {
+        opacity: 1,
+        x: 0
+    },
+    "-=0.5"
 );
+
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true
+});
