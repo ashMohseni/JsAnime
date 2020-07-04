01d7acc204bae10793b11b3a9ebc5a4ccd254d84
Ashkan Mohseni <amohseni80@yahoo.com>
2020-07-04 15:53:16 -0400

    Updates the animation for the second page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const hero = document.querySelector('.hero');
+const hero2 = document.querySelector('#hero2');
 const slider = document.querySelector('.slider');
 const logo = document.querySelector('#logo');
 const menu = document.querySelector('.menu');
@@ -68,5 +69,38 @@ tl.fromTo(
 
 new fullpage('#fullpage', {
     autoScrolling: true,
-    navigation: true
+    navigation: true,
+
+  onLeave: function(origin, destination, direction){
+    const tl2 = new TimelineMax();
+
+
+
+    if (destination.index === 1) {
+      tl2.fromTo(
+          hero2,
+          0, {
+              height: "10%"
+          }, {
+              height: '80%',
+              ease: Power2.easeInOut
+          }
+      ).fromTo(
+          hero2,
+          0, {
+              width: "100%"
+          }, {
+              width: "80%",
+              position: "relative",
+              left: "10",
+              transform: "translate(10%)",
+              ease: Power2.easeInOut
+          }
+      )
+    }
+
+
+  }
+
+
 });
